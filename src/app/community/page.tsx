66dada2c41e628a5f2371c17454940d7f79ed954
stopbrother <stopbro0323@naver.com>
2024-10-11 22:35:20 +0900

    add: 커뮤니티(게시판) 구조 만들기

---
+++ src/app/community/page.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+const Communitypage = () => {
+  return (
+    <main>
+      <section className="flex flex-col">
+        <h1>자유 게시판</h1>
+        <hr />
+        <div>
+          <ul>
+            <div className="flex flex-row justify-between">
+              <p>제목</p>
+              <p>작성자</p>
+              <p>작성일</p>
+            </div>
+            <li className="flex flex-row justify-between">
+              <Link href="/community/1">
+                <p>제목이요</p>
+              </Link>
+              <p>작성자요</p>
+              <p>작성일이요</p>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+        <button>글 작성</button>
+      </section>
+    </main>
+  );
+};
+
+export default Communitypage;
---
+++ src/app/community/write/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import NoSsrEditor from '@/components/community/NoSsrEditor';
+
+const CommunityWritepage = () => {
+  return (
+    <div className="w-[768px] m-auto">
+      <NoSsrEditor />
+    </div>
+  );
+};
+
+export default CommunityWritepage;
