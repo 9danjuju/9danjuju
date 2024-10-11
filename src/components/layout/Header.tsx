29453ecce4e46ff4117933419d50a17e47ed56e7
dajung <kwonkkan@naver.com>
2024-10-11 16:03:24 +0900

    초기 세팅

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="w-full h-8 flex justify-between px-10">
+      <p className="font-bold">구단주주총회</p>
+      <div className="flex gap-5">
+        <p>회원가입</p>
+        <p>로그인</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
