911eeecedc19b90fa20b98ec6949bd24e106d584
GiSeong Lee <cj8928@gmail.com>
2024-10-12 13:46:52 +0900

    update: 헤더 라우팅 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <div className="w-full h-8 flex justify-between px-10">
       <p className="font-bold">구단주주총회</p>
       <div className="flex gap-5">
-        <p>회원가입</p>
-        <p>로그인</p>
+        <Link href="/signup">회원가입</Link>
+        <Link href="login">로그인</Link>
       </div>
     </div>
   );
