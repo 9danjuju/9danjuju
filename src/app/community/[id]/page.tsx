223e6c795112ce6bd01695a34e7bae546a05763c
stopbrother <stopbro0323@naver.com>
2024-10-15 00:02:29 +0900

    update: 게시물 상세페이지 데이터 보여주기

--- src/app/community/[id]/page.tsx
+++ src/app/community/[id]/page.tsx
@@ -1,5 +1,32 @@
-const CommunityDetailpage = () => {
-  return <div>page</div>;
+import EditDeleteButton from '@/components/community/EditDeleteButton';
+import browserClient from '@/utils/supabase/client';
+
+interface CommunityDetailPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const CommunityDetailpage = async ({ params }: CommunityDetailPageProps) => {
+  const { data: postData } = await browserClient.from('Post').select().eq('id', params.id).single();
+  if (!postData) return <div>해당 게시물이 없습니다.</div>;
+  return (
+    <div>
+      <section key={postData.id}>
+        <div>
+          <h1>{postData.title}</h1>
+          <div>
+            <div>
+              <p>{postData.date}</p>| <p>{postData.userNickname}</p>
+            </div>
+
+            <EditDeleteButton postId={postData.id} />
+          </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.content }}></div>
+      </section>
+    </div>
+  );
 };
 
 export default CommunityDetailpage;
