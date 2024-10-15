d62c26f10c4cb11693710d594c40f96bcdaae89e
GiSeong Lee <cj8928@gmail.com>
2024-10-15 16:16:42 +0900

    add: meta data를 가져오는 쿼리 생성

--- src/utils/client-action.ts
+++ src/utils/client-action.ts
@@ -1,8 +1,8 @@
-import { MatchDetailType } from '@/types/matchType';
+import { MatchDetailType, SpidPlayerType, SppositionType } from '@/types/matchType';
 
-export const getMatchId = async (nickname: string, matchtype: number) => {
+export const getMatchId = async (nickname: string, matchtype: number, offset: number) => {
   try {
-    const response = await fetch(`/api/matchId?nickname=${nickname}&matchtype=${matchtype}`, {
+    const response = await fetch(`/api/matchId?nickname=${nickname}&matchtype=${matchtype}&offset=${offset}`, {
       method: 'GET'
     });
     const data: string[] = await response.json();
@@ -21,5 +21,31 @@ export const getMatchDetail = async (matchid: string) => {
     return data;
   } catch (error) {
     throw new Error(`Error: ${error}`);
+  }
+};
+
+export const getPlayerPosition = async () => {
+  try {
+    const res = await fetch(`/api/playerposition/detail`);
+    if (!res.ok) {
+      throw new Error(`서버 응답 없음: ${res.status}`);
+    }
+    const playerPositionData: SppositionType[] = await res.json();
+    return playerPositionData;
+  } catch (error) {
+    console.error('선수 포지션 데이터 오류', error);
+  }
+};
+
+export const getPlayerNames = async () => {
+  try {
+    const res = await fetch(`/api/playerdata/detail`);
+    if (!res.ok) {
+      throw new Error(`서버 응답 없음: ${res.status}`);
+    }
+    const playerNameRes: SpidPlayerType[] = await res.json();
+    return playerNameRes;
+  } catch (error) {
+    console.error('선수 이름 데이터 오류', error);
   }
 };
