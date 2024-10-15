4439a959cd6815fde189e7f4e7599e9169e38b90
GiSeong Lee <cj8928@gmail.com>
2024-10-15 16:16:22 +0900

    update: 전적 가져오기 더보기 버튼 useInfiniteQuery를 통해 구현

--- src/hooks/useGetMatchIdQuery.ts
+++ src/hooks/useGetMatchIdQuery.ts
@@ -1,9 +1,17 @@
 import { getMatchId } from '@/utils/client-action';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-export const useGetMatchIdQuery = (nickname: string, matchType: number) => {
-  return useQuery({
-    queryKey: ['matchid', nickname],
-    queryFn: () => getMatchId(nickname, matchType)
+export const useGetMatchIdQueryInfiniteQuery = (nickname: string, matchType: number) => {
+  return useInfiniteQuery({
+    queryKey: ['matchid', nickname, matchType],
+    queryFn: ({ pageParam = 0 }) => getMatchId(nickname as string, matchType, pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length === 2) {
+        return allPages.length * 2;
+      } else {
+        return undefined;
+      }
+    }
   });
 };
