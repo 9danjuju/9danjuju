cc79d406ac8f54739fc98b15ddad524748b545df
star1024cd <1star1024cd@gmail.com>
2024-10-15 16:10:27 +0900

    updata : 매치별 선수 정보 모달 추가

--- src/app/detail/[nickname]/page.tsx
+++ src/app/detail/[nickname]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import PlayerImage from '@/components/detail/PlayerImage';
-import { AllMatchType, MatchDetailType, SppositionType, UserInfoType } from '@/types/matchType';
+import { AllMatchType, MatchDetailType, PlayerInfoType, SppositionType, UserInfoType } from '@/types/matchType';
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
@@ -34,8 +34,20 @@ export default function Page({ params }: { params: ParamsType }) {
   const [playerNames, setPlayerNames] = useState<{ id: number; name: string }[]>([]);
   const [playerPosition, setPlayerPosition] = useState<SppositionType[]>([]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPlayer, setSelectedPlayer] = useState<PlayerInfoType | null>(null);
   const searchParams = useSearchParams();
   const nickname = searchParams.get('nickname');
+
+  const openModal = (selectedPlayer: PlayerInfoType) => {
+    setSelectedPlayer(selectedPlayer);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedPlayer(null);
+  };
 
   // 유저정보
   const fetchUserData = async (nickname: string) => {
@@ -281,7 +293,7 @@ export default function Page({ params }: { params: ParamsType }) {
 
             {/* 상세창 토글 */}
             {!isToggle[index] ? (
-              <div className="flex flex-col justify-between items-center  bg-green-300 text-white max-w-7xl w-full mx-auto">
+              <div className="flex flex-col justify-between items-center text-white max-w-7xl w-full mx-auto">
                 <nav className="bg-pink-400 flex items-center justify-between m-2 p-5 gap-2 text-xl max-w-7xl w-full mx-auto">
                   <span>평점</span>
                   <span>종합</span>
@@ -289,40 +301,157 @@ export default function Page({ params }: { params: ParamsType }) {
                   <span>패스</span>
                   <span>수비</span>
                 </nav>
-                <div className="bg-green-400 flex flex-row justify-between m-2 p-5 gap-2 text-xl max-w-7xl w-full mx-auto">
+
+                <div className="bg-gray-300 flex flex-row justify-between m-2 p-5 gap-2 text-xl max-w-7xl w-full mx-auto">
                   {matchDetail.matchInfo.map((info, index) => (
                     <div
                       key={index}
                       className={`flex-1 flex flex-col ${index === 0 ? 'items-start' : 'items-end border-l-2'}`}
                     >
+                      {/* 선수목록 */}
                       {info.player
                         .filter((player) => player.status.spRating > 0)
                         .map((player, playerIndex) => (
-                          <div key={playerIndex} className="mb-3 w-28">
-                          
-                              <PlayerImage spId={player.spId} spRating={player.status.spRating} />
-                     
+                          <div key={playerIndex} onClick={() => openModal(player)} className="mb-3 w-28">
+                            <PlayerImage spId={player.spId} spRating={player.status.spRating} />
                             <div className="bg-red-500 text-white p-1 rounded-lg text-center font-bold">
                               {getPlayerPosition(player.spPosition, playerPosition)}
                             </div>
                             <div className="bg-zinc-800 text-white p-1 rounded-lg text-center">
                               {getPlayerName(player.spId, playerNames)}
                             </div>
-                            <div>{player.spGrade}등급</div>
-                            <div>
-                              패스성공률:
-                              {isNaN(Math.round((player.status.passSuccess / player.status.passTry) * 100))
-                                ? 0
-                                : Math.round((player.status.passSuccess / player.status.passTry) * 100)}
-                              %
-                            </div>
-                            <div>득점: {player.status.goal}</div>
                           </div>
                         ))}
                     </div>
                   ))}
                 </div>
                 <div></div>
+                {isModalOpen && selectedPlayer && (
+                  <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-white p-4 rounded shadow-lg w-1/4 text-black  text-center">
+                      <div className="flex flex-row">
+                        {/* 선수기본정보 */}
+                        <div className="p-5">
+                          {/* 선수이름 */}
+                          <h3 className="text-2xl font-bold mt-2">
+                            {selectedPlayer && getPlayerName(selectedPlayer.spId, playerNames)}
+                          </h3>
+                          <div className="flex flex-row justify-center items-center">
+                            <span>포지션</span>
+                            <span className="font-bold">
+                              {selectedPlayer && getPlayerPosition(selectedPlayer.spPosition, playerPosition)}
+                            </span>
+                            <span>강화등급</span>
+                            <span className="bg-slate-200 rounded-sm w-4">{selectedPlayer?.spGrade}</span>
+                          </div>
+                        </div>
+                        {/* 선수이미지 */}
+                        <div>
+                          <PlayerImage spId={selectedPlayer.spId} spRating={selectedPlayer.status.spRating} />
+                        </div>
+                      </div>
+                      <div>
+                        {/* 주요지표 */}
+                        <h2>주요지표</h2>
+                        <div className=" bg-gray-200 p-5">
+                          <div className="flex justify-between">
+                            <span>득점</span>
+                            <span>{selectedPlayer.status.goal}</span>
+                          </div>
+                          <div className="flex justify-between">
+                            <span>어시스트</span>
+                            <span>{selectedPlayer.status.assist}</span>
+                          </div>
+                          <div className="flex justify-between">
+                            <span>패스성공률</span>
+                            <span>
+                              {isNaN(
+                                Math.round((selectedPlayer.status.passSuccess / selectedPlayer.status.passTry) * 100)
+                              )
+                                ? 0
+                                : Math.round((selectedPlayer.status.passSuccess / selectedPlayer.status.passTry) * 100)}
+                              %
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        {/* 주요지표 */}
+                        <h2>공격지표</h2>
+                        <div className=" bg-gray-200 p-5">
+                          <div className="flex justify-between">
+                            <span>슈팅정확도</span>
+                            <span>
+                              {isNaN(
+                                Math.round((selectedPlayer.status.effectiveShoot / selectedPlayer.status.shoot) * 100)
+                              )
+                                ? 0
+                                : Math.round(
+                                    (selectedPlayer.status.effectiveShoot / selectedPlayer.status.shoot) * 100
+                                  )}
+                              %
+                            </span>
+                          </div>
+
+                          <div className="flex justify-between">
+                            <span>빗나간슈팅</span>
+                            <span>{selectedPlayer.status.shoot - selectedPlayer.status.effectiveShoot}</span>
+                          </div>
+
+                          <div className="flex justify-between">
+                            <span>유효 슈팅</span>
+                            <span>{selectedPlayer.status.effectiveShoot}</span>
+                          </div>
+
+                          <div className="flex justify-between">
+                            <span>전체 슛</span>
+                            <span>{selectedPlayer.status.shoot}</span>
+                          </div>
+                        </div>
+
+                        <div>
+                          {/* 주요지표 */}
+                          <h2>공통지표</h2>
+                          <div className=" bg-gray-200 p-5">
+                            <div className="flex justify-between">
+                              <span>패스 성공률</span>
+                              <span>
+                                {isNaN(
+                                  Math.round((selectedPlayer.status.passSuccess / selectedPlayer.status.passTry) * 100)
+                                )
+                                  ? 0
+                                  : Math.round(
+                                      (selectedPlayer.status.passSuccess / selectedPlayer.status.passTry) * 100
+                                    )}
+                                %
+                              </span>
+                            </div>
+                            <div className="flex justify-between">
+                              <span>패스 시도</span>
+                              <span>{selectedPlayer.status.passTry}</span>
+                            </div>
+                            <div className="flex justify-between">
+                              <span>패스 성공</span>
+                              <span>{selectedPlayer.status.passSuccess}</span>
+                            </div>
+                            <div className="flex justify-between">
+                              <span>드리블 시도</span>
+                              <span>{selectedPlayer.status.dribbleTry}</span>
+                            </div>
+                            <div className="flex justify-between">
+                              <span>드리블 성공</span>
+                              <span>{selectedPlayer.status.dribbleSuccess}</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <button onClick={closeModal} className="mt-4 bg-red-500 text-white p-2 rounded">
+                        닫기
+                      </button>
+                    </div>
+                  </div>
+                )}
                 {/* <Field /> */}
                 {/* <div className="bg-orange-400 flex flex-row justify-between m-2 p-5 gap-2 text-xl max-w-7xl w-full mx-auto">
                   여기스코어보드
