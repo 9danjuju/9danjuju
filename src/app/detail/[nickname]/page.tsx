d52de7427a93b6708768489e3d48b56c92188f22
GiSeong Lee <cj8928@gmail.com>
2024-10-16 15:29:09 +0900

    style: 전적 상단 유저정보 상자 스타일 변경

--- src/app/detail/[nickname]/page.tsx
+++ src/app/detail/[nickname]/page.tsx
@@ -1,6 +1,7 @@
 import MatchContainer from '@/components/detail/MatchContainer';
 import { getFifaUserInfo } from '@/utils/server-action';
-import { utcTimeToKstConverter } from '@/utils/utcTimeToKstConverter';
+import { utcTimeToKstConverter } from '@/utils/services/utcTimeToKstConverter';
+import Image from 'next/image';
 
 interface ParamsType {
   nickname: string;
@@ -12,16 +13,17 @@ const Detail = async ({ params }: { params: ParamsType }) => {
     <div className="flex flex-col justify-center items-center">
       <div className="bg-gray-200 flex justify-center items-center m-2 p-5 max-w-3xl w-full mx-auto">
         {userData ? (
-          <div className="flex w-full justify-between items-center">
-            <div className="flex flex-col">
+          <div className="flex justify-between items-center w-full">
+            <div className="flex flex-col items-center justify-center w-1/2">
+              <Image src={'/img/anonPlayerImage.png'} width={100} height={100} alt="profile" />
               <h1 className="text-2xl">{userData.nickname}</h1>
               <p>Level: {userData.level}</p>
             </div>
 
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-end">
               {userData.maxDivision.map((division) => {
                 return (
-                  <div key={division.achievementDate} className="my-5">
+                  <div key={division.achievementDate} className="mt-5">
                     <p>
                       {division.matchType}: {division.division}
                     </p>
