cddf29cf1395f08410c34aceb4580c2b1aa8a498
stopbrother <stopbro0323@naver.com>
2024-10-16 23:25:59 +0900

    style: Home 페이지 style 추가(투명도) & 문구 추가

--- src/components/community/HomeCommunityList.tsx
+++ src/components/community/HomeCommunityList.tsx
@@ -5,7 +5,7 @@ const HomeCommunityList = async () => {
   const { data } = await browserClient.from('Post').select().order('date', { ascending: false }).range(0, 2);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <div className="flex flex-row justify-between">
         <h2 className="font-bold text-xl">커뮤니티</h2>
         <Link href="/community">더보기</Link>
@@ -14,10 +14,10 @@ const HomeCommunityList = async () => {
         {data?.map((post) => (
           <li
             key={post.id}
-            className="grid grid-cols-[2fr_1fr_1fr] gap-3 p-3 h-14 text-center leading-loose bg-[#D9D9D9]"
+            className="grid grid-cols-[2fr_1fr_1fr] gap-3 p-3 h-14 text-center font-semibold leading-loose bg-[#ffffff] bg-opacity-40 rounded-xl"
           >
             <Link href={`/community/${post.id}`}>
-              <div className="text-left">{post.title}</div>
+              <div className="text-left hover:text-[#0a1215]">{post.title}</div>
             </Link>
             <div>{post.userNickname}</div>
             <div>{new Date(post.date).toLocaleString('ko-KR')}</div>
