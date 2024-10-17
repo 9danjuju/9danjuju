2ee569517559a549dd1483031e1e6fe2d9c7cf87
stopbrother <stopbro0323@naver.com>
2024-10-17 12:51:59 +0900

    update: 상세페이지 클라이언트 컴포넌트로 변경

--- src/app/community/[id]/page.tsx
+++ src/app/community/[id]/page.tsx
@@ -1,6 +1,8 @@
+'use client';
 import Comment from '@/components/comments/Comment';
 import CommunityActionButton from '@/components/community/CommunityActionButton';
 import browserClient from '@/utils/supabase/client';
+import { useEffect, useState } from 'react';
 
 interface CommunityDetailPageProps {
   params: {
@@ -8,22 +10,39 @@ interface CommunityDetailPageProps {
   };
 }
 
-const CommunityDetailpage = async ({ params }: CommunityDetailPageProps) => {
-  const { data: postData } = await browserClient.from('Post').select().eq('id', params.id).single();
-  if (!postData) return <div>해당 게시물이 없습니다.</div>;
+const CommunityDetailpage = ({ params }: CommunityDetailPageProps) => {
+  const [postData, setPostData] = useState<{
+    content: string;
+    date: string;
+    id: string;
+    title: string;
+    user_id: string;
+    userNickname: string | null;
+  } | null>(null);
+
+  const getData = async () => {
+    const { data } = await browserClient.from('Post').select().eq('id', params.id).single();
+    console.log('DetailPage', data);
+    setPostData(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  if (!postData) return <div>..Loading</div>;
   return (
     <div className="w-full">
-      <section key={postData.id} className="flex flex-col gap-4 mx-auto mt-10 py-4 max-w-[1000px]">
-        <h1 className="text-2xl">{postData.title}</h1>
+      <section key={postData?.id} className="flex flex-col gap-4 mx-auto mt-10 py-4 max-w-[1000px]">
+        <h1 className="text-2xl">{postData?.title}</h1>
         <div className="flex flex-row justify-between pb-4 border-b border-l-neutral-700">
           <div className="flex flex-row gap-2 text-sm">
-            <p>{new Date(postData.date).toLocaleString('ko-KR')}</p> | <p>{postData.userNickname}</p>
+            <p>{new Date(postData!.date).toLocaleString('ko-KR')}</p> | <p>{postData?.userNickname}</p>
           </div>
 
-          <CommunityActionButton postId={postData.id} createUserId={postData.user_id} />
+          <CommunityActionButton postId={postData?.id} createUserId={postData!.user_id} />
         </div>
 
-        <div dangerouslySetInnerHTML={{ __html: postData.content }} className="py-6"></div>
+        <div dangerouslySetInnerHTML={{ __html: postData!.content }} className="py-6"></div>
       </section>
       <Comment />
     </div>
